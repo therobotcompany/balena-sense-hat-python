c5ab7aef42a78d033223c21d733224cd91f0d2be
Bernardo Ronquillo <bernardo.ronquillo@universaldx.com>
2020-09-06 13:25:53 +0200

    start.sh with joystick events in the LED matrix & enviromental conditions in the container's log

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 #./scripts/rainbow.py
 
 echo "Move the joystick to check that I'm alive..."
-./scripts/joystick_events.py
+./scripts/joystick_events.py &
 
 echo "In parallel watch the environment conditions inthe container's log..."
-./weather_station.py &
+./weather_station.py
